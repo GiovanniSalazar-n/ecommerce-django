a7791b6298d49b1318b2911b9ecb6aa2a754e22b
GiovanniSalazar-n <gial210802@gmail.com>
2026-02-20 23:46:36 -0600

    Intro a REST

--- src/product/urls.py
+++ src/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_detail
+from .views import product_detail, ProductAPIView
 
 app_name = "product"
 
 urlpatterns = [
     path("<int:pk>/", product_detail, name="detail"),
+    path("api/", ProductAPIView.as_view(), name="api"),
 ]
--- src/product/views.py
+++ src/product/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
+from rest_framework import views
+from rest_framework.response import Response
+
+
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
@@ -12,3 +16,26 @@ def product_detail(request, pk):
         "divisible_num": 2,
     }
     return render(request, "ecommerce/product_detail.html", context)
+
+
+class ProductAPIView(views.APIView):
+
+    def get(self, request):
+        print(">>> Estás llamando el método GET")
+        return Response({"message": "Estás llamando el método GET"})
+
+    def post(self, request):
+        print(">>> Estás llamando el método POST")
+        return Response({"message": "Estás llamando el método POST"})
+
+    def put(self, request):
+        print(">>> Estás llamando el método PUT")
+        return Response({"message": "Estás llamando el método PUT"})
+
+    def patch(self, request):
+        print(">>> Estás llamando el método PATCH")
+        return Response({"message": "Estás llamando el método PATCH"})
+
+    def delete(self, request):
+        print(">>> Estás llamando el método DELETE")
+        return Response({"message": "Estás llamando el método DELETE"})
